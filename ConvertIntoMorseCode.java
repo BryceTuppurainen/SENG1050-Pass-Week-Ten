62997ac6526fbb019f7636daf8c531eed80601f4
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2021-10-11 10:46:06 +1100

    Updated ConvertIntoMorseCode to Match Video

--- ConvertIntoMorseCode.java
+++ ConvertIntoMorseCode.java
@@ -2,33 +2,32 @@ import java.util.Scanner;
 
 public class ConvertIntoMorseCode {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
+        Scanner con = new Scanner(System.in);
+        
         System.out.print("Type a message $ ");
-        String message_in_english = console.nextLine().toLowerCase();
-        String message_in_morse_code = "";
-        for (int iter = 0; iter < message_in_english.length(); iter++) {
-            switch (message_in_english.charAt(iter)) {
-                case 'a':
-                    message_in_morse_code += ".- ";
-                    break;
-                case 'b':
-                    message_in_morse_code += "-... ";
-                    break;
-                case 'c':
-                    message_in_morse_code += "-.-. ";
-                    break;
-                case 'd':
-                    message_in_morse_code += "-.. ";
-                    break;
-                case 'e':
-                    message_in_morse_code += ". ";
-                    break;
-                default:
-                    message_in_morse_code += message_in_english.charAt(iter);
-                    break;
-            }
+        String english = con.nextLine().toLowerCase();
+        String morse = "";
+
+        String[] englishCharacters = {"a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        String[] morseCharacters = {".- ", "-... ", "-.-.","-.. ",". ","..-. ","--. ",".... ",".. ",".--- ","-.- ",".-.. ","-- ","-. ","--- ",".--. ","--.- ",".-. ","... ","- ","..- ","...- ",".-- ","-..- ","-.-- ","--.. "};
+
+        for (int iter = 0; iter < english.length(); iter++) {
+            int cI = getIndex(englishCharacters, english.substring(iter, iter+1));
+            if (cI == -1) morse += english.substring(iter, iter+1);
+            else {
+                morse += morseCharacters[cI];
+            } 
         }
-        System.out.println(message_in_morse_code);
-        console.close();
+
+        System.out.println(morse);
+
+        con.close();
+    }
+
+    public static int getIndex(String[] arr, String target) {
+        for (int iter = 0; iter < arr.length; iter++) {
+            if (arr[iter].equals(target)) return iter;
+        }
+        return -1;
     }
 }
--- GenerateChecksum.java
+++ GenerateChecksum.java
@@ -3,32 +3,23 @@ import java.lang.Math;
 
 public class GenerateChecksum {
     public static void main(String[] args) {
-        Scanner console = new Scanner(System.in);
-        String hex = "";
-        while (hex.length() == 0 || hex.length()%2 == 1) {
-            System.out.print("Input a hex number (even): ");
-            hex = console.nextLine();
-            if (hex.length()%2 == 1) System.out.println("ERROR - The value was not even, please enter another value...");
-        }
+        Scanner con = new Scanner(System.in);
 
-        System.out.println("Decimal conversion: "+hexToDec(hex));
+        System.out.print("Input a hex number $ ");
+        String hexIn = con.nextLine().toUpperCase();
 
-        console.close();
+        System.out.print("Checksum for "+hexIn+" is equal to (DEC) "+(256 - hexToDec(hexIn)));
+        
+        con.close();
     }
 
-    private static String decToHex(int val) {
-        return "";
-    }
-
-    private static int hexToDec(String val) {
+    private static String decToHex(int decVal) {
+        String dec = String.valueOf(decVal);
         int output = 0;
-        for (int iter = 0 ; iter < val.length(); iter++) {
-            System.out.println(val.charAt(iter));
-            switch (val.charAt(iter)) {
-                case '0':
-                    break;
+        for (int iter = 0; iter < dec.length(); iter++) {
+            switch (dec.charAt(dec.length()-1-iter)) {
                 case '1':
-                    output += Math.pow(16, iter);
+                    output += 1 * Math.pow(16, iter);
                     break;
                 case '2':
                     output += 2 * Math.pow(16, iter);
@@ -36,10 +27,101 @@ public class GenerateChecksum {
                 case '3':
                     output += 3 * Math.pow(16, iter);
                     break;
+                case '4':
+                    output += 4 * Math.pow(16, iter);
+                    break;
+                case '5':
+                    output += 5 * Math.pow(16, iter);
+                    break;
+                case '6':
+                    output += 6 * Math.pow(16, iter);
+                    break;
+                case '7':
+                    output += 7 * Math.pow(16, iter);
+                    break;
+                case '8':
+                    output += 8 * Math.pow(16, iter);
+                    break;
+                case '9':
+                    output += 9 * Math.pow(16, iter);
+                    break;
+                case 'A':
+                    output += 10 * Math.pow(16, iter);
+                    break;
+                case 'B':
+                    output += 11 * Math.pow(16, iter);
+                    break;
+                case 'C':
+                    output += 12 * Math.pow(16, iter);
+                    break;
+                case 'D':
+                    output += 13 * Math.pow(16, iter);
+                    break;
+                case 'E':
+                    output += 14 * Math.pow(16, iter);
+                    break;
+                case 'F':
+                    output += 15 * Math.pow(16, iter);
+                    break;
                 default:
                     break;
             }
-            System.out.println(output);
+        }
+        return output;
+    }
+
+    private static int hexToDec(String hex) {
+        int output = 0;
+        for (int iter = 0; iter < hex.length(); iter++) {
+            switch (hex.charAt(hex.length()-1-iter)) {
+                case '1':
+                    output += 1 * Math.pow(16, iter);
+                    break;
+                case '2':
+                    output += 2 * Math.pow(16, iter);
+                    break;
+                case '3':
+                    output += 3 * Math.pow(16, iter);
+                    break;
+                case '4':
+                    output += 4 * Math.pow(16, iter);
+                    break;
+                case '5':
+                    output += 5 * Math.pow(16, iter);
+                    break;
+                case '6':
+                    output += 6 * Math.pow(16, iter);
+                    break;
+                case '7':
+                    output += 7 * Math.pow(16, iter);
+                    break;
+                case '8':
+                    output += 8 * Math.pow(16, iter);
+                    break;
+                case '9':
+                    output += 9 * Math.pow(16, iter);
+                    break;
+                case 'A':
+                    output += 10 * Math.pow(16, iter);
+                    break;
+                case 'B':
+                    output += 11 * Math.pow(16, iter);
+                    break;
+                case 'C':
+                    output += 12 * Math.pow(16, iter);
+                    break;
+                case 'D':
+                    output += 13 * Math.pow(16, iter);
+                    break;
+                case 'E':
+                    output += 14 * Math.pow(16, iter);
+                    break;
+                case 'F':
+                    output += 15 * Math.pow(16, iter);
+                    break;
+                default:
+                    break;
+            }
         }
         return output;
     }
